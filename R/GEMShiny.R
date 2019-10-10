df25a18f548cf3360c533dc19915d825ad1e78c6
unknown <gabrielleoconnor@ozemail.com.au>
2019-10-10 08:43:07 +0800

    Broke down GEM into different preprocessing functions

--- R/GEMShiny.R
+++ R/GEMShiny.R
@@ -1,29 +1,57 @@
-
-
-# Return column names of specified path
-
-
-getColNames <- function(fileName){
- names(fread(fileName,nrows=1,header=TRUE))
-}
+# Package development and Roxygen 2
 
 library(sva)
 library(data.table)
 
+
+#' shinyGEM_Emodel
+#'
+#' GEM_Emodel is to find the association between methylation and environmental factor genome widely.
+#'
+#' GEM_Emodel adjusts the methylation data for batch effect and then finds the association between methylation and environment genome-wide by performing matrix
+#' based iterative correlation and memory-efficient data analysis instead of millions of linear regressions
+#' (N = number_of_CpGs). The methylation data are the measurements for CpG probes, for example, 450,000 CpGs
+#' from Illumina Infinium HumanMethylation450 Array. The environmental factor can be a particular phenotype or environment
+#' factor from,for example, birth outcomes, maternal conditions or disease traits. The output of GEM_Emodel
+#' for particular environmental factor is a list of CpGs that are potential epigenetic biomarkers.
+#' GEM_Emodel runs linear regression like lm (M ~ E + covt), where M is a matrix with methylation data,
+#' E is a matrix with environment factor and covt is a matrix with covariates, and all read from the
+#' formatted text data file.
+#'
+#'
+#' @param envFileName A csv or txt file name of demographic and phenotypical data with columns representing environmental factors and columns representing sample
+#' @param methylFileName A csv or txt file of methylation data with the CpG sites in the rows and the samples across the columns
+#' @param batchName = "Plate_No" a variable containing the name of the batch effect variable used for the ComBat methylation
+#' @param predictorName = "Smoke" a variable continaing the name of the target environmental factor
+#' @param covName Either a single string or a vector of string containing the column names included in the regression model
+#' @param outputFileName = "GemEmodelOutput" a file name used as the output of the funciton. All results will be written to a file with this name.
+#'
+#'
+#' @return output a list of values for the
+#' @export
+#'
+#'
+#'
 ### FileStructure
 # Methyl cpg sites in the rows and samples across the columns
 # Environment file is enviromental factors in the columns and samples down the rows
-GEM_Emodel <- function(envFileName, methylFileName , batchName = "Plate_no",
-                       predictorName= "Smoke",covName= NULL, outputFileName = "GemEmodelOutput.csv", fileDelimiter = "\t"){
+shinyGEM_Emodel <- function(envFileName, methylFileName , batchName = "Plate_no",
+                       predictorName= "Smoke",covName= NULL, outputFileName = "GemEmodelOutput.csv"){
+  # Read in environmental data
   envData = fread(envFileName,header=TRUE)
+  # Read in methylation data
   methylData = fread(methylFileName)
-  ## ComBat adjustment
-  methComBat = ComBat(dat=methylData[-1,-1], batch = as.factor(data.frame(envData)[,batchName] ),par.prior =TRUE);
+  # Calling batchAdjust function which implements the ComBat method
+  methComBat <- batchAdjust(envData,methylData,batchName)
+  # Setting up for matrix eQTL package
   errorCovariance = numeric();
-
+  # Setting target environmental factor
   env <- data.frame(envData)[,predictorName]
+  # Setting covariance variable, if none set set cvrt to null
   if (length(covName) > 0) {
     cvrt <- data.frame(envData)[,covName]
+  }else{
+        cvrt <- NULL
       }
 
 
@@ -62,74 +90,4 @@ GEM_Emodel <- function(envFileName, methylFileName , batchName = "Plate_no",
 }
 
 
-
-
-GEM_Emodel <-
-  function(env_file_name, covariate_file_name, methylation_file_name,
-           Emodel_pv, output_file_name, qqplot_file_name, fileDelimiter = "\t") {
-
-    errorCovariance = numeric();
-
-    env <- SlicedData$new();
-    env$fileDelimiter = fileDelimiter;      # default the TAB character
-    env$fileOmitCharacters = "NA"; # denote missing values;
-    env$fileSkipRows = 1;          # one row of column labels
-    env$fileSkipColumns = 1;       # one column of row labels
-    env$fileSliceSize = 2000;      # read file in slices of 2,000 rows
-    env$LoadFile(env_file_name);
-
-    cvrt <- SlicedData$new();
-    cvrt$fileDelimiter = fileDelimiter;      # default the TAB character
-    cvrt$fileOmitCharacters = "NA"; # denote missing values;
-    cvrt$fileSkipRows = 1;          # one row of column labels
-    cvrt$fileSkipColumns = 1;       # one column of row labels
-    if (length(covariate_file_name) > 0) {
-      cvrt$LoadFile(covariate_file_name);
-    }
-
-    cpg = SlicedData$new();
-    cpg$fileDelimiter = fileDelimiter;      # default the TAB character
-    cpg$fileOmitCharacters = "NA"; # denote missing values;
-    cpg$fileSkipRows = 1;          # one row of column labels
-    cpg$fileSkipColumns = 1;       # one column of row labels
-    cpg$fileSliceSize = 2000;      # read file in slices of 2,000 rows
-    cpg$LoadFile(methylation_file_name);
-
-    ## Run the analysis
-    Emodel <- Matrix_eQTL_engine2(
-      snps = env,
-      gene = cpg,
-      cvrt = cvrt,
-      output_file_name =  NULL,
-      pvOutputThreshold = Emodel_pv,
-      useModel = modelLINEAR,
-      errorCovariance = errorCovariance,
-      verbose = FALSE,
-      pvalue.hist = "qqplot",
-      min.pv.by.genesnp = FALSE,
-      noFDRsaveMemory = FALSE,
-      addInfo = "CpGs"
-    )
-
-    unlink(output_file_name);
-    ## Results:
-    cat('Analysis done in: ', Emodel$time.in.sec, ' seconds', '\n');
-    #show(Emodel$all$eqtls)
-    #R2 = Emodel$all$eqtls$statistic ^ 2 / (Emodel$all$eqtls$statistic ^ 2 + Emodel$param$dfFull);
-    result_Emodel <- cbind(
-      as.character(Emodel$all$eqtls$gene),
-      Emodel$all$eqtls$beta,
-      Emodel$all$eqtls$statistic,
-      Emodel$all$eqtls$pvalue,
-      Emodel$all$eqtl$FDR
-    )
-    colnames(result_Emodel) <- c("cpg", "beta", "stats", "pvalue", "FDR")
-    write.table( result_Emodel, output_file_name, sep = "\t", row.names = F, quote = F)
-
-
-
-    jpeg(qqplot_file_name, width = 2000, height = 2000, res = 300)
-    plot(Emodel, pch = 16, cex = 0.7);
-    dev.off()
-  }
 
