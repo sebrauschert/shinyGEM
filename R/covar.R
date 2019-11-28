e6662e5e09572a973c343690b0a39b2f643e115c
Hobbeist <hobbeist@googlemail.com>
2019-11-28 15:10:37 +0800

    added example data

---
+++ R/covar.R
@@ -0,0 +1,20 @@
+#' Simulated covariaten data set with 11 variables.
+#'
+#' A dataset containing 11 simulated variables and 987 rows
+#'
+#' @format A data frame with 987 rows and 11 columns:
+#' \describe{
+#'   \item{ID}{An ID column, numers from 1 to 987}
+#'   \item{sex}{A variable representing biological sex, coded as 0 and 1}
+#'   \item{BMI}{A randomly generated body mass index (BMI) variable}
+#'   \item{preg_smoking}{A randomly generated variable for exposure to maternal smoking, coded as 0 and 1}
+#'   \item{Gran}{Randomly generated granulocyte cell counts}
+#'   \item{Mono}{Randomly generated monocytes cell counts}
+#'   \item{Bcell}{Randomly generated beta cells cell counts}
+#'   \item{NK}{Randomly generated natural killer cells cell counts}
+#'   \item{CD4T}{Randomly generated CD4T cell counts}
+#'   \item{CD8T}{Randomly generated CD8T cell counts}
+#'   \item{Plate_no}{Randomly generated batch variable}
+#' }
+#'
+"covar"
---
+++ R/meth.R
@@ -0,0 +1,11 @@
+#' Simulated methylation data set with 1000 CpGs.
+#'
+#' A dataset containing a simulated subset of 1000 CpGs from the Illumina HumanMethylation450 array
+#'
+#' @format A data frame with 1000 rows and 988 columns:
+#' \describe{
+#'   \item{cgXXXXXX}{DNA methylation beta}
+#'   ...
+#' }
+#'
+"meth"
